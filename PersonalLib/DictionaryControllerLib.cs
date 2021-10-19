bdaa4e9988a4217815ca530f64751f851ba92bd6
closethereport <closethereport@gmail.com>
2021-10-19 15:35:42 +0300

    Init

---
+++ PersonalLib/DictionaryControllerLib.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PersonalLib
+{
+    public class DictionaryControllerLib
+    {
+        private Dictionary<string, int> CalculateWords(string[] contents)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var word in contents)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                else
+                {
+                    dict[word] = 1;
+                }
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ UniqWordsV.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using PersonalLib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace UniqWordsV._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var input = new StreamReader("RawFile.txt"))
+            {
+                var contents = input.ReadToEnd()
+                                                .ToLower()
+                                                .Replace("/[0-9]", "")
+                                                .Replace(",", "")
+                                                .Replace(".", "")
+                                                .Replace("(", "")
+                                                .Replace(")", "")
+                                                .Replace(".", "")
+                                                .Replace("-", "")
+                                                .Replace(":", "")
+                                                .Replace("  ", "")
+                                                .Split(' ');
+
+                var CalculateWords = typeof(DictionaryControllerLib).GetMethod("CalculateWords", BindingFlags.NonPublic | BindingFlags.Instance);
+                var dict = (Dictionary<string, int>)CalculateWords.Invoke(new DictionaryControllerLib(), new object[] { contents });
+                
+                var ordered = from k in dict.Keys
+                              orderby dict[k] descending
+                              select k;
+                using (StreamWriter output = new StreamWriter("output.txt"))
+                {
+                    foreach (var k in ordered)
+                    {
+                        output.WriteLine(string.Format("{0}:  {1}", k, dict[k]));
+                    }
+                }
+            }
+        }
+    }
+}
